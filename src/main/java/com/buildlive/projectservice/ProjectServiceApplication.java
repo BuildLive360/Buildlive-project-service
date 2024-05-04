7d217d768860ba00f62d7bb7366f968a17b5e571
Aravind <aravindmunna240@gmail.com>
2024-05-04 14:04:08 +0530

    "actions added new one"

--- src/main/java/com/buildlive/projectservice/ProjectServiceApplication.java
+++ src/main/java/com/buildlive/projectservice/ProjectServiceApplication.java
@@ -18,18 +18,18 @@ import java.io.IOException;
 public class ProjectServiceApplication {
 
 
-	@Bean
-	FirebaseMessaging firebaseMessaging() throws IOException {
-		FileInputStream serviceAccount =
-				new FileInputStream("D:\\BuildLive\\BuildLive-back\\project-service\\src\\main\\resources\\notification-a8cf1-firebase-adminsdk-945ae-d6775e7bfc.json");
-
-		FirebaseOptions options = new FirebaseOptions.Builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.build();
-		if(FirebaseApp.getApps().isEmpty()){
-			FirebaseApp.initializeApp(options);
-		}
-		return FirebaseMessaging.getInstance();
+//	@Bean
+//	FirebaseMessaging firebaseMessaging() throws IOException {
+//		FileInputStream serviceAccount =
+//				new FileInputStream("D:\\BuildLive\\BuildLive-back\\project-service\\src\\main\\resources\\notification-a8cf1-firebase-adminsdk-945ae-d6775e7bfc.json");
+//
+//		FirebaseOptions options = new FirebaseOptions.Builder()
+//				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//				.build();
+//		if(FirebaseApp.getApps().isEmpty()){
+//			FirebaseApp.initializeApp(options);
+//		}
+//		return FirebaseMessaging.getInstance();
 
 //		GoogleCredentials googleCredentials = GoogleCredentials.fromStream(
 //				new ClassPathResource("notification-a8cf1-firebase-adminsdk-945ae-d6775e7bfc.json").getInputStream());
@@ -37,7 +37,7 @@ public class ProjectServiceApplication {
 //				.setCredentials(googleCredentials).build();
 //		FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions,"my-app");
 //		return FirebaseMessaging.getInstance(app);
-	}
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectServiceApplication.class, args);
